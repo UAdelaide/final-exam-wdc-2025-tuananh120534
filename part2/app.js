712cc2a3e2e4084684136dde02a385f34e86f05e
tuananh120534 <ndtanh.2005@gmail.com>
2025-06-20 09:14:31 +0000

    [automatic commit] 2025-06-20 09:14:31 UTC 149.167.27.153

--- part2/app.js
+++ part2/app.js
@@ -10,6 +10,13 @@ const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
 
+app.use(session({
+  secret: 'your-secret',
+  resave: false,
+  saveUninitialized: false
+}));
+
+
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
 const userRoutes = require('./routes/userRoutes');

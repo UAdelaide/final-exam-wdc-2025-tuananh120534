cdb9986f1be5fd5ff93ada577a2d1972895a578a
tuananh120534 <ndtanh.2005@gmail.com>
2025-06-20 04:51:33 +0000

    [automatic commit] 2025-06-20 04:51:33 UTC 129.127.202.124

--- part2/app.js
+++ part2/app.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const path = require('path');
 require('dotenv').config();
-const loginRoute = require('./routes/login');
 
 const app = express();
 
 // Middleware
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
-app.use('/login', loginRoute);
 
 // Routes
 const walkRoutes = require('./routes/walkRoutes');

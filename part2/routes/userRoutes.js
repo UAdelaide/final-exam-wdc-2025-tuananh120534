68e42f75be4e502d7f0238a6a75ece56dedcacb0
tuananh120534 <ndtanh.2005@gmail.com>
2025-06-20 09:27:17 +0000

    [automatic commit] 2025-06-20 09:27:17 UTC 149.167.27.153

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -37,22 +37,25 @@ router.get('/me', (req, res) => {
 
 // POST login (dummy version)
 router.post('/login', async (req, res) => {
-  const { email, password } = req.body;
+  const { username, password } = req.body;
 
   try {
     const [rows] = await db.query(`
       SELECT user_id, username, role FROM Users
-      WHERE email = ? AND password_hash = ?
-    `, [email, password]);
+      WHERE username = ? AND password_hash = ?
+    `, [username, password]);
 
     if (rows.length === 0) {
       return res.status(401).json({ error: 'Invalid credentials' });
     }
+
+    req.session.user = rows[0];// Store in session
 
     res.json({ message: 'Login successful', user: rows[0] });
   } catch (error) {
     res.status(500).json({ error: 'Login failed' });
   }
 });
+
 
 module.exports = router;

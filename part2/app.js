1e8f6d1a6ab472e7361d4fe14cf12cb5dad7598b
tuananh120534 <ndtanh.2005@gmail.com>
2025-06-20 09:37:21 +0000

    [automatic commit] 2025-06-20 09:37:21 UTC 149.167.27.153

--- part2/app.js
+++ part2/app.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const path = require('path');
 require('dotenv').config();
-const session = require('express-session');
-
 
 const app = express();
 
@@ -10,28 +8,12 @@ const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
 
-app.use(session({
-  secret: 'your-secret',
-  resave: false,
-  saveUninitialized: false
-}));
-
-const username = form.username.value;
-const password = form.password.value;
-
-
-
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
 const userRoutes = require('./routes/userRoutes');
 
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
-
-app.get('/', (req, res) => {
-  res.redirect('/login.html');
-});
-
 
 // Export the app instead of listening here
 module.exports = app;

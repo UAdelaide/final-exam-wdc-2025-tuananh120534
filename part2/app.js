f204efa3171548b90d30c05c8fbcec2e7f640871
tuananh120534 <ndtanh.2005@gmail.com>
2025-06-20 14:46:41 +0000

    [automatic commit] 2025-06-20 14:46:41 UTC 149.167.27.153

--- part2/app.js
+++ part2/app.js
@@ -20,7 +20,7 @@ const userRoutes = require('./routes/userRoutes');
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
 
-
+app.use('/api/dogs', dogsRouter);
 
 // Export the app instead of listening here
 module.exports = app;

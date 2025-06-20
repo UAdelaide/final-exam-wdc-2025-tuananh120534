a4907c5dd917569c877c141739951923f5b491a3
tuananh120534 <ndtanh.2005@gmail.com>
2025-06-20 11:44:02 +0000

    [automatic commit] 2025-06-20 11:44:02 UTC 149.167.27.153

--- part2/app.js
+++ part2/app.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const path = require('path');
 require('dotenv').config();
-
+const session = require('express-session');
 const app = express();
 
 // Middleware
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
-
+app.use(session({
+  secret: 'your-secret-key', // change this to a secure secret
+  resave: false,
+  saveUninitialized: false,
+}));
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
 const userRoutes = require('./routes/userRoutes');

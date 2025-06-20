f7765ac80e0a9bf2d83783793860864ee5b8ae91
tuananh120534 <ndtanh.2005@gmail.com>
2025-06-20 06:09:45 +0000

    [automatic commit] 2025-06-20 06:09:45 UTC 129.127.202.124

--- part2/app.js
+++ part2/app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const path = require('path');
 require('dotenv').config();
-
+const session = require('express-session');
+const mysql = require('mysql2/promise');
 const app = express();
 
 // Middleware

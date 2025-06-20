a3d46ca3f28bab25e46bfc0aaed40d3c1a7cc8be
tuananh120534 <ndtanh.2005@gmail.com>
2025-06-20 03:43:42 +0000

    [automatic commit] 2025-06-20 03:43:42 UTC 129.127.202.124

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -76,7 +76,7 @@ INSERT INTO Dogs (owner_id, name, size) VALUES
 
 INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status) VALUES
  ((SELECT dog_id FROM Dogs WHERE name='Max'), '2025-06-10 08:00:00', 30, 'Parklands', 'open'),
- ((SELECT dog_id FROM Dogs WHERE name='Rocky'), '2025-06-11 7:00:00', 15, 'Zoo', 'open'),
+ ((SELECT dog_id FROM Dogs WHERE name='Rocky'), '2025-06-15 7:00:00', 15, 'Zoo', 'open'),
  ((SELECT dog_id FROM Dogs WHERE name='Bella'), '2025-06-10 09:30:00', 45, 'Beachside Ave', 'accepted'),
- ((SELECT dog_id FROM Dogs WHERE name='Milo'), '2025-06-12 10:30:00', 20, 'Rundle Mall', 'open'),
+ ((SELECT dog_id FROM Dogs WHERE name='Milo'), '2025-06-16 10:30:00', 20, 'Rundle Mall', 'open'),
  ((SELECT dog_id FROM Dogs WHERE name='Luna'), '2025-06-12 12:00:00', 60, 'Botanic Garden', 'accepted');

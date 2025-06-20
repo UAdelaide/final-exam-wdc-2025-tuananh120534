8adb7d66d75e4849fb4916df8602513c03c9f2df
tuananh120534 <ndtanh.2005@gmail.com>
2025-06-20 13:08:47 +0000

    [automatic commit] 2025-06-20 13:08:47 UTC 149.167.27.153

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -67,7 +67,15 @@ router.post('/login', async (req, res) => {
     res.status(500).json({ error: 'Login failed' });
   }
 });
-
+router.post('/logout', (req, res) => {
+  req.session.destroy((err) => { // <-- add parentheses around err
+    if (err) {
+      return res.status(500).json({ error: 'Logout failed' });
+    }
+    res.clearCookie('connect.sid');
+    res.json({ message: 'Logged out' });
+  });
+});
 
 
 

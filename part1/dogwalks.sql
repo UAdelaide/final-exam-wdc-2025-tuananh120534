340b967e13ef5ccca311465359bf679a8e24f9d3
tuananh120534 <ndtanh.2005@gmail.com>
2025-06-20 02:25:26 +0000

    [automatic commit] 2025-06-20 02:25:26 UTC 129.127.202.124

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -58,8 +58,8 @@ INSERT INTO Users (username, email, password_hash, role) VALUES
   ('alice123', 'alice@example.com', 'hashed123', 'owner'),
   ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
   ('carol123', 'carol@example.com', 'hashed789', 'owner'),
-  ('davewalk', 'dave@example.com', 'hashed111', 'walker'),
-  ('eveowner', 'eve@example.com', 'hashed222', 'owner');
+  ('davewalk', 'dave@example.com', 'hashed1234', 'walker'),
+  ('eveowner', 'eve@example.com', 'hashed12345', 'owner');
 
 INSERT INTO Dogs (owner_id, name, size) VALUES
   ((SELECT user_id FROM Users WHERE username='alice123'), 'Max', 'medium'),

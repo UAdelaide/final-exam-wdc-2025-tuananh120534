2730ed4e27dd10097e4a7bce13a35a1d2d5889c5
tuananh120534 <ndtanh.2005@gmail.com>
2025-06-20 05:44:13 +0000

    [automatic commit] 2025-06-20 05:44:13 UTC 129.127.202.124

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -78,4 +78,4 @@ VALUES
  ((SELECT dog_id FROM Dogs WHERE name = 'Thinh'), '2025-06-9 10:30:00', 45, 'Zoo', 'accepted'),
  ((SELECT dog_id FROM Dogs WHERE name = 'Bella'), '2025-06-10 08:45:00', 60, 'Beachside Ave', 'open'),
  ((SELECT dog_id FROM Dogs WHERE name = 'Thai'), '2025-06-11 17:15:00', 40, 'Botanic Garden', 'open'),
- ((SELECT dog_id FROM Dogs WHERE name = 'Khai'), '2025-06-12 11:00:00', 30, 'Adelaide University', 'open');
+ ((SELECT dog_id FROM Dogs WHERE name = 'Khai'), '2025-06-12 11:00:00', 30, 'Beach', 'open');

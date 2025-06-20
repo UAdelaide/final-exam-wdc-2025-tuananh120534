e16a494428f5a0344a2e88e8b873cb48a8860dbe
tuananh120534 <ndtanh.2005@gmail.com>
2025-06-20 10:39:29 +0000

    [automatic commit] 2025-06-20 10:39:29 UTC 149.167.27.153

--- part2/app.js
+++ part2/app.js
@@ -4,16 +4,6 @@ require('dotenv').config();
 
 const app = express();
 
-const session = require('express-session');
-
-app.use(session({
-  secret: 'supersecuresecret', // Change in production
-  resave: false,
-  saveUninitialized: false,
-  cookie: { secure: false } // set to true only if using HTTPS
-}));
-
-
 // Middleware
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
@@ -21,9 +11,7 @@ app.use(express.static(path.join(__dirname, '/public')));
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
 const userRoutes = require('./routes/userRoutes');
-const authRoutes = require('./routes/authRoutes');
 
-app.use('/api/auth', authRoutes);
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
 

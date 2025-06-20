4eba97f6791c9d1c8b56577e588568a4c9805365
tuananh120534 <ndtanh.2005@gmail.com>
2025-06-20 09:29:18 +0000

    [automatic commit] 2025-06-20 09:29:18 UTC 149.167.27.153

--- part2/app.js
+++ part2/app.js
@@ -10,7 +10,6 @@ const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
 
-const session = require('express-session');
 app.use(session({
   secret: 'your-secret',
   resave: false,
